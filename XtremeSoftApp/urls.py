f36384675b5d71af9ef82c69e4d37af9fbba4fbc
Pablo RB <prodriguezbravo@safareyes.es>
2024-04-22 21:11:57 +0200

    Mostrar poductos + Carrusel Inicio

--- XtremeSoftApp/urls.py
+++ XtremeSoftApp/urls.py
@@ -4,5 +4,6 @@ from .views import *
 urlpatterns = [
     path('inicio/', go_home, name='inicio'),
     path('nuevo_producto/', crear_producto, name='crear_producto'),
+    path('lista_productos/', mostrar_productos, name='mostrar_productos'),
 
 ]
--- XtremeSoftApp/views.py
+++ XtremeSoftApp/views.py
@@ -1,9 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import *
 
 # Create your views here.
 
 def go_home (request):
     return render(request,'inicio.html')
 
+def mostrar_productos(request):
+    return render(request, "productos.html")
+
 def crear_producto (request):
-    return render(request,'crear_producto.html')
+    if request.method=="GET":
+        lista_productos=Producto.objects.all()
+        return render(request,'productos.html', {'productos':lista_productos})
+    else:
+        nuevo_producto = Producto()
+        nuevo_producto.nombre=request.POST.get("name")
+        nuevo_producto.descripcion = request.POST.get("descripcion")
+        nuevo_producto.precio = float(request.POST.get("price"))
+        nuevo_producto.foto = request.POST.get("image")
+        nuevo_producto.save()
+        return redirect("/lista_productos")
+
+
